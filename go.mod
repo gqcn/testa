ae71594ad20950dc4f99a9c970279e5314650e1e
John <john@johng.cn>
2019-09-09 13:57:12 +0800

    init

--- go.mod
+++ go.mod
@@ -1,5 +1,2 @@
 module github.com/johngcn/testa
 
-go 1.12
-
-require github.com/gogf/gf v1.5.16
--- testa.go
+++ testa.go
@@ -2,13 +2,8 @@ package testa
 
 import (
     "fmt"
-    "github.com/gogf/gf/g"
 )
 
 func TestA() {
-    err := g.DB().PingMaster()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("testa ping master ok")
+    fmt.Println("Test A!")
 }
